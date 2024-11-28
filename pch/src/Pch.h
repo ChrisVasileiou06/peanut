c37b2d5e819f018880755c6356efbb5c6efc44f9
Christos Vasileiou <ChrisVasileiou06@gmail.com>
2024-11-28 23:12:12 +0200

    initial commit

---
+++ pch/src/Pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_PCH_H_
+#define PCH_PCH_H_
+
+#include <cstdlib>
+#include <cstdint>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <list>
+
+#endif // PCH_PCH_H_
